ebafa8db5eca07b21d80ecc97b6b0f0f8ea652c7
Satish <sawantss.77@gmail.com>
2018-03-28 19:04:55 +0530

    Homework Details Commit

--- src/main/java/drogenidesoftwares/drogenideschool/homework/HomeworkDetailsActivity.java
+++ src/main/java/drogenidesoftwares/drogenideschool/homework/HomeworkDetailsActivity.java
@@ -1,8 +1,20 @@
 package drogenidesoftwares.drogenideschool.homework;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import drogenidesoftwares.drogenideschool.R;
+
 /**
  * Created by pc1 on 3/28/2018.
  */
 
-public class HomeworkDetailsActivity {
+public class HomeworkDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homework_details_layout);
+
+    }
 }
